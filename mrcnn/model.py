a5e7b114ae61c2b1a3fee3a01d0cb0801788182a
Tom Gross <itconsense@gmail.com>
2019-12-30 15:26:48 +0100

    allow single process call

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2393,7 +2393,7 @@ class MaskRCNN(object):
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
+            workers=1,   # XXX
             use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
